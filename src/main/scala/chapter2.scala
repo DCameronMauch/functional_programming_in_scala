f3a59d0487d9f1241650f0ac0800c0056e4b860f
D Cameron Mauch <cam@douglas-cameron.net>
2018-05-04 11:34:39 -0600

    updated exercise descriptions

--- src/main/scala/chapter2.scala
+++ src/main/scala/chapter2.scala
@@ -1,8 +1,11 @@
 object chapter2 {
-  // Exercise 2.1
+  // Exercise 1
   // calculate nth Fibonacci number using a local an annotated tail-recursive function
 
   def fib(n: Int): Int = ???
+
+  // Exercise 2
+  // check if array of type A is sorted given an ordering function
 
   def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = ???
 }
