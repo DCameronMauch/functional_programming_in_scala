30f51d7b027976aa4fc9085f9106192108213e0f
D Cameron Mauch <cam@douglas-cameron.net>
2018-05-04 10:20:53 -0600

    chapter 1 exercise 1 implementation

--- src/main/scala/chapter2.scala
+++ src/main/scala/chapter2.scala
@@ -2,5 +2,16 @@ object chapter2 {
   // Exercise 2.1
   // calculate nth Fibonacci number using a local tail-recursive function
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(a: Int, b: Int, rem: Int): Int = {
+      if (rem == 0) b else go(b, a + b, rem - 1)
+    }
+
+    n match {
+      case 0 => 0
+      case x if x < 3 => 1
+      case _ => go(1, 2, n - 3)
+    }
+  }
 }
