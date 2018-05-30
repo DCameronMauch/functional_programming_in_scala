ff08e76e39de10e292c1048de6ecc8b1856b74c0
D Cameron Mauch <cam@douglas-cameron.net>
2018-05-30 13:21:50 -0600

    working on chapter 3

--- src/main/scala/chapter3.scala
+++ src/main/scala/chapter3.scala
@@ -20,6 +20,20 @@ object List {
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail:_*))
 
+  def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
+    case Nil => a2
+    case Cons(h, t) => Cons(h, append(t, a2)) }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f)) }
+
+  def sum2(ns: List[Int]): Int =
+    foldRight(ns, 0)((x ,y) => x + y)
+
+  def product2(ns: List[Double]): Double =
+    foldRight(ns, 1.0)(_ * _)
+
   // Exercise 2
   // write a tail function that returns the list without the first element
 
@@ -40,4 +54,9 @@ object List {
   // while a provided function evaluates to true
 
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+
+  // Exercise 6
+  // write a init function to return list with last element removed
+
+  def init[A](l: List[A]): List[A] = ???
 }
